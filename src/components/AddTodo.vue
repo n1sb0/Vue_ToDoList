e0b7a5123506fd222e54b10f7e6a0e84bcff282a
Bohdan Sivak <bohdan.sivak@gmail.com>
2020-10-23 11:04:16 +0200

    1 Vue.js project todoList

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="Add Todo..."
+      />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+//import uuid from "uuid";
+//import { v4 as uuidv4 } from "uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+
+      const newTodo = {
+        // id: uuidv4(),
+        title: this.title,
+        completed: false,
+      };
+      // Send up to perent
+      this.$emit("add-todo", newTodo);
+
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  border-bottom: 1px #485460 solid;
+}
+
+input {
+  text-decoration: none;
+  outline: none;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+  border: none;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/Todoitem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="todo-item" v-bind:class="{ 'is-complete': todo.completed }">
+    <p>
+      <input type="checkbox" id="checkbox" v-on:change="markComplete" />
+      {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #ecf0f1;
+  padding: 10px;
+  border-bottom: 1px #95a5a6 solid;
+}
+
+.todo-item:hover {
+  background: #353b48;
+  color: #ecf0f1;
+}
+
+.is-complete {
+  text-decoration: line-through;
+  background: #2ecc71;
+}
+
+.del {
+  background: #e74c3c;
+  border: 0ch;
+  color: #fff;
+  padding: 5px 9px;
+  border-radius: 25%;
+  cursor: pointer;
+  float: right;
+  font-size: 1.5vh;
+}
+
+.del:hover {
+  background: #c23616;
+}
+</style>
